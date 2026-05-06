274b733400d163d2e04ce991a94c02dbf2dcf705
nefertariraj <mrajomarison@gmail.com>
2026-05-06 10:21:59 +0300

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -106,7 +106,7 @@ else:
         tabs = st.tabs(["DEFINE", "MEASURE", "ANALYZE", "IMPROVE", "CONTROL"])
 
     # IMPORTANT : Cette ligne doit être décalée vers la droite (alignée avec tabs)
-    with tabs[0]:
+        with tabs[0]:
     # --- PHASE DEFINE ---
     with tabs[0]:
         st.header("Phase Define")
