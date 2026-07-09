c24e933d06f841ba45cdbfe53ac6c9333e5b4f6e
nefertariraj <mrajomarison@gmail.com>
2026-07-09 15:25:12 +0300

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -2144,12 +2144,8 @@ else:
                     p_master_save_key = f"save_master_config_{var_clean_id}_{safe_idx}"
                     if p_master_save_key not in p:
                         p[p_master_save_key] = {}
+                    # On met à jour uniquement les valeurs modifiées par l'utilisateur
                     p[p_master_save_key].update(master_cfg)
-                    
-                    # On s'assure de débloquer le protocole et de marquer le statut validé dans p et session_state
-                    p["protocol_saved"] = True
-                    p[f"validated_status_{var_clean_id}_{safe_idx}"] = True
-                    st.session_state[f"status_lock_{var_clean_id}_{safe_idx}"] = True
                         
                 # --- BOUTON DÉDIÉ : LANCER L'ANALYSE DES BIAIS ---
                 st.markdown("<br>", unsafe_allow_html=True)
