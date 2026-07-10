cff3ca3aa574ba2fe3f03f36b04c58bf58a8cffc
nefertariraj <mrajomarison@gmail.com>
2026-07-10 15:37:55 +0300

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -2173,7 +2173,7 @@ else:
                         if isinstance(p, dict):
                             p[save_key] = updated_data.copy()
                             
-                        st.success("🎯 La totalité du formulaire (type et valeurs associées) est enregistrée et sauvegardée dans le JSON avec succès !")
+                        st.success("🎯 La totalité du formulaire est enregistrée et sauvegardée avec succès !") 
                         
                 # --- BOUTON DÉDIÉ : LANCER L'ANALYSE DES BIAIS ---
                 st.markdown("<br>", unsafe_allow_html=True)
